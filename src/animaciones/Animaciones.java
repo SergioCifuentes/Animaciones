a6875a1033db22fb59a84cb291994d50596ab335
Sergio Cifuentes <sergiocifuentes1485@gmail.com>
2020-05-19 01:37:52 -0600

    Creacion y Editacion de Archivos

--- src/animaciones/Animaciones.java
+++ src/animaciones/Animaciones.java
@@ -5,6 +5,8 @@
  */
 package animaciones;
 
+import animaciones.ui.IDE;
+
 /**
  *
  * @author sergio
@@ -15,7 +17,9 @@ public class Animaciones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       IDE ide = new IDE();
+       ide.setVisible(true);
+               
     }
     
 }
